cb1b0fd62472c1612323dc2280c0b459f0311f48
Venu <vgpoloju@gmail.com>
2020-07-04 10:17:32 +0530

    Update CallingActivity.java
    package name change

--- src/android/CallingActivity.java
+++ src/android/CallingActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
-import com.heka.mobile.R;
+import com.heka.ios.R;
 
 import org.json.JSONObject;
 import java.security.cert.CertificateException;
